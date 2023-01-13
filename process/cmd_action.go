d8f32565e531f142da7eacc84331161a239457ac
phidiaLam <2957035701@qq.com>
2023-01-13 16:07:30 +0800

    👔 实现与qq频道进行简单交互

---
+++ process/cmd_action.go
@@ -0,0 +1,42 @@
+package process
+
+import (
+	"context"
+	"log"
+
+	"github.com/tencent-connect/botgo/dto"
+)
+
+func (p Processor) setEmoji(ctx context.Context, channelID string, messageID string) {
+	err := p.Api.CreateMessageReaction(
+		ctx, channelID, messageID, dto.Emoji{
+			ID:   "307",
+			Type: 1,
+		},
+	)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (p Processor) setPins(ctx context.Context, channelID, msgID string) {
+	_, err := p.Api.AddPins(ctx, channelID, msgID)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (p Processor) setAnnounces(ctx context.Context, data *dto.WSATMessageData) {
+	if _, err := p.Api.CreateChannelAnnounces(
+		ctx, data.ChannelID,
+		&dto.ChannelAnnouncesToCreate{MessageID: data.ID},
+	); err != nil {
+		log.Println(err)
+	}
+}
+
+func (p Processor) sendReply(ctx context.Context, channelID string, toCreate *dto.MessageToCreate) {
+	if _, err := p.Api.PostMessage(ctx, channelID, toCreate); err != nil {
+		log.Println(err)
+	}
+}
---
+++ process/process.go
@@ -0,0 +1,182 @@
+package process
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/tencent-connect/botgo/dto"
+	"github.com/tencent-connect/botgo/dto/message"
+	"github.com/tencent-connect/botgo/openapi"
+)
+
+// Processor is a struct to process message
+type Processor struct {
+	Api openapi.OpenAPI
+}
+
+// ProcessMessage is a function to process message
+func (p Processor) ProcessMessage(input string, data *dto.WSATMessageData) error {
+	ctx := context.Background()
+	cmd := message.ParseCommand(input)
+	toCreate := &dto.MessageToCreate{
+		Content: "默认回复" + message.Emoji(307),
+		MessageReference: &dto.MessageReference{
+			// 引用这条消息
+			MessageID:             data.ID,
+			IgnoreGetMessageError: true,
+		},
+	}
+
+	// 进入到私信逻辑
+	if cmd.Cmd == "dm" {
+		p.dmHandler(data)
+		return nil
+	}
+
+	switch cmd.Cmd {
+	case "hi":
+		p.sendReply(ctx, data.ChannelID, toCreate)
+	case "time":
+		toCreate.Content = genReplyContent(data)
+		p.sendReply(ctx, data.ChannelID, toCreate)
+	case "ark":
+		toCreate.Ark = genReplyArk(data)
+		p.sendReply(ctx, data.ChannelID, toCreate)
+	case "公告":
+		p.setAnnounces(ctx, data)
+	case "pin":
+		if data.MessageReference != nil {
+			p.setPins(ctx, data.ChannelID, data.MessageReference.MessageID)
+		}
+	case "emoji":
+		if data.MessageReference != nil {
+			p.setEmoji(ctx, data.ChannelID, data.MessageReference.MessageID)
+		}
+	default:
+	}
+
+	return nil
+}
+
+// ProcessInlineSearch is a function to process inline search
+func (p Processor) ProcessInlineSearch(interaction *dto.WSInteractionData) error {
+	if interaction.Data.Type != dto.InteractionDataTypeChatSearch {
+		return fmt.Errorf("interaction data type not chat search")
+	}
+	search := &dto.SearchInputResolved{}
+	if err := json.Unmarshal(interaction.Data.Resolved, search); err != nil {
+		log.Println(err)
+		return err
+	}
+	if search.Keyword != "test" {
+		return fmt.Errorf("resolved search key not allowed")
+	}
+	searchRsp := &dto.SearchRsp{
+		Layouts: []dto.SearchLayout{
+			{
+				LayoutType: 0,
+				ActionType: 0,
+				Title:      "内联搜索",
+				Records: []dto.SearchRecord{
+					{
+						Cover: "https://pub.idqqimg.com/pc/misc/files/20211208/311cfc87ce394c62b7c9f0508658cf25.png",
+						Title: "内联搜索标题",
+						Tips:  "内联搜索 tips",
+						URL:   "https://www.qq.com",
+					},
+				},
+			},
+		},
+	}
+	body, _ := json.Marshal(searchRsp)
+	if err := p.Api.PutInteraction(context.Background(), interaction.ID, string(body)); err != nil {
+		log.Println("api call putInteractionInlineSearch  error: ", err)
+		return err
+	}
+	return nil
+}
+
+func (p Processor) dmHandler(data *dto.WSATMessageData) {
+	dm, err := p.Api.CreateDirectMessage(
+		context.Background(), &dto.DirectMessageToCreate{
+			SourceGuildID: data.GuildID,
+			RecipientID:   data.Author.ID,
+		},
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	toCreate := &dto.MessageToCreate{
+		Content: "默认私信回复",
+	}
+	_, err = p.Api.PostDirectMessage(
+		context.Background(), dm, toCreate,
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func genReplyContent(data *dto.WSATMessageData) string {
+	var tpl = `你好：%s
+在子频道 %s 收到消息。
+收到的消息发送时时间为：%s
+当前本地时间为：%s
+消息来自：%s
+`
+
+	msgTime, _ := data.Timestamp.Time()
+	return fmt.Sprintf(
+		tpl,
+		message.MentionUser(data.Author.ID),
+		message.MentionChannel(data.ChannelID),
+		msgTime, time.Now().Format(time.RFC3339),
+	)
+}
+
+func genReplyArk(data *dto.WSATMessageData) *dto.Ark {
+	return &dto.Ark{
+		TemplateID: 23,
+		KV: []*dto.ArkKV{
+			{
+				Key:   "#DESC#",
+				Value: "这是 ark 的描述信息",
+			},
+			{
+				Key:   "#PROMPT#",
+				Value: "这是 ark 的摘要信息",
+			},
+			{
+				Key: "#LIST#",
+				Obj: []*dto.ArkObj{
+					{
+						ObjKV: []*dto.ArkObjKV{
+							{
+								Key:   "desc",
+								Value: "这里展示的是 23 号模板",
+							},
+						},
+					},
+					{
+						ObjKV: []*dto.ArkObjKV{
+							{
+								Key:   "desc",
+								Value: "这是 ark 的列表项名称",
+							},
+							{
+								Key:   "link",
+								Value: "https://www.qq.com",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
